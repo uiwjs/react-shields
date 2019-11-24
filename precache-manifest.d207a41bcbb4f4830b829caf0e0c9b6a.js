d28e000b10dce67fe3b081f48d4f4cacc00e3701
jaywcjlove <jaywcjlove@users.noreply.github.com>
2019-11-24 07:45:17 +0000

    Automated deployment: Sun Nov 24 07:45:17 UTC 2019 70948bf2e5393ec3555d300792e1467fbf5da689

---
+++ precache-manifest.d207a41bcbb4f4830b829caf0e0c9b6a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cff457928e57a86468eefa5e35d328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33da03ebf6da5b87258f",
+    "url": "/static/css/main.0aecc3b4.css"
+  },
+  {
+    "revision": "33da03ebf6da5b87258f",
+    "url": "/static/js/main.185311a1.js"
+  },
+  {
+    "revision": "b8ad4d392ce9dea3dd98fea1c6850263",
+    "url": "/static/media/logo.b8ad4d39.svg"
+  }
+]);
